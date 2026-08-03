6fb91dee6842728893daa85d0866896c2ce11d6d
Rodrigo de Oliveira <rodrigodotnet@gmail.com>
2026-08-03 18:46:39 -0300

    declare claims attribute usage

--- src/WebApiCoreSeed.Api/Attributes/ClaimsAuthorizeAttribute.cs
+++ src/WebApiCoreSeed.Api/Attributes/ClaimsAuthorizeAttribute.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.CompilerServices;
 using Microsoft.AspNetCore.Mvc;
 using WebApiCoreSeed.Api.Extensions.Clains;
@@ -5,6 +6,7 @@ using WebApiCoreSeed.Api.Filters;
 
 namespace WebApiCoreSeed.Api.Attributes
 {
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public class ClaimsAuthorizeAttribute : TypeFilterAttribute
     {
         public ClaimsAuthorizeAttribute(string claimName, [CallerMemberName] string claimValue = "") : base(typeof(RequisitoClaimFilter))
